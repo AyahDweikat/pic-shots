86486dc1a734d27537ac0f0fd8d00f924fdc9de2
ayahdweikat <dweikatayah99@gmail.com>
2023-01-09 23:34:55 +0200

    some-edits

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import { GlobalContext } from "../../Context/Context";
 import { apiKey, ImageSearch } from "../../Utils/ApiUtils";
 import "./Home.scss";
+import { getFirstImg } from './../../Utils/ApiUtils';
 
 function Home() {
   const [query, setQuery] = useState("mountains");
@@ -9,80 +10,135 @@ function Home() {
   const [errorMsg, setErrorMsg] = useState("");
   const [images, setImages] = useState([]);
   const [flag, setFlag] = useState(false);
+  const [flagSug, setFlagSug] = useState(true);
+  const [suggArr, setSugg] = useState([
+    "mountains",
+    "flowers",
+    "snow",
+    "rivers",
+    "computer",
+    "sites",
+    "travel",
+    "london",
+    "paris",
+    "roma",
+    "newyork",
+  ]);
   const [lovedImg, setLovedImg] = useState([]);
   const [userInfo, setUserInfo] = useState({});
+  const [suggestions, setSuggestions] = useState([{}]);
+
   function searchData(e) {
     setQuery(e.target.value);
   }
 
-  async function getImages(e) {
+  async function getImages(e, item, num) {
     e.preventDefault();
     setQuery("");
     setImages([]);
-    let data = await ImageSearch(query, 100, apiKey);
-    if (data.stat === "ok" && data.photos.pages) {
-      data.photos.photo.map((item)=>{
+    setSuggestions([]);
+    setFlagSug(false);
+    let { data, status } = await ImageSearch(item, num, apiKey);
+    if (status === 200 && data.pages) {
+      data.photo.map((item) => {
         item.flagIcons = false;
-      })
+      });
       setFlag(true);
-      setImages(data.photos.photo);
+      setImages(data.photo);
     } else {
       setFlag(false);
       setNoImgsMsg("No Results coming from this search");
     }
   }
-  function loved(id){
-    if(!userInfo){
-      alert('You are not login, please log in so you can saved images in loved folder');
 
-      setErrorMsg("You are not login, please log in so you can saved images in loved folder");
+  async function getSuggest(){
+    const resultArray = await Promise.all(suggArr.map(async (name) => {
+      return {
+        name: name,
+        data: await ImageSearch(name, 1, apiKey)
+      }
+    }
+    ));
+    let _arr= await resultArray.map((item)=>{
+      return {
+        name: item.name,
+        pic : item.data.data.photo[0]
+      }
+    });
+    setSuggestions(_arr)
+  }
+
+  useEffect(()=>{
+    getSuggest();
+    setFlagSug(true);
+  },[])
+  console.log(suggestions);
+
+
+
+
+
+
+
+  function loved(id) {
+    if (!userInfo) {
+      alert(
+        "You are not login, please log in so you can saved images in loved folder"
+      );
+
+      setErrorMsg(
+        "You are not login, please log in so you can saved images in loved folder"
+      );
       return 0;
     }
     let _images = [...images];
     let _lovedImg = [...lovedImg];
-    _images.map((pic)=>{
-      if(pic.id === id){
-        if(pic.flagIcons === false){
+    _images.map((pic) => {
+      if (pic.id === id) {
+        if (pic.flagIcons === false) {
           _lovedImg.push(pic);
           setLovedImg(_lovedImg);
-        } 
-        else 
-        {
-          let __filteredArr = _lovedImg.filter((item)=>{
-            return item.id !==id;
-          })
+        } else {
+          let __filteredArr = _lovedImg.filter((item) => {
+            return item.id !== id;
+          });
           setLovedImg(__filteredArr);
         }
-        return pic.flagIcons = !pic.flagIcons;
+        return (pic.flagIcons = !pic.flagIcons);
       }
-    })
+    });
     setImages(_images);
   }
-  useEffect(()=>{
+  useEffect(() => {
     const _userInfo = JSON.parse(localStorage.getItem("userinfo"));
     setUserInfo(_userInfo);
-    if(userInfo.name === "ayah"){
+    if (userInfo.name === "ayah") {
       setErrorMsg("");
     }
-    return ()=>{
-      let state = lovedImg.length ? true: false;
-      let obj = {state , data:lovedImg};
+    return () => {
+      let state = lovedImg.length ? true : false;
+      let obj = { state, data: lovedImg };
       localStorage.setItem("lovedImgs", JSON.stringify(obj));
-    }
+    };
   }, [lovedImg]);
 
   return (
-      <div className={flag ? "pt-5 homePage" : "pt-5 homePage1"}>
-        <div className="pt-5 container ">
-          <form className="searchForm" onSubmit={getImages}>
-            <input type="text" value={query} onChange={searchData} placeholder="Search for Images"/>
-            <button type="submit">Search</button>
-          </form>
-          <div>
-            <p>{errorMsg}</p>
-          </div>
-          <div className="images row m-auto mt-3">
-            {flag ?
+    <div className={flag ? "pt-5 homePage" : "pt-5 homePage1"}>
+      <div className="pt-5 container ">
+        <form className="searchForm" onSubmit={(e) => getImages(e, query, 100)}>
+          <input
+            type="text"
+            value={query}
+            onChange={searchData}
+            placeholder="Search for Images"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <div>
+          <p>{errorMsg}</p>
+        </div>
+        <div className="images row m-auto mt-3">
+          {flag ? (
             images.map((pic, idx) => {
               return (
                 <div className="col-md-3 imageCard" key={idx}>
@@ -92,19 +148,46 @@ function Home() {
                       alt={pic.title}
                     />
                   </div>
-                  <button className="loveBtn" onClick={()=>loved(pic.id)}>
-                    {pic.flagIcons ? <i className="fa-solid fa-heart"></i>
-                    :<i className="fa-regular fa-heart"></i>
-                    }
+                  <button className="loveBtn" onClick={() => loved(pic.id)}>
+                    {pic.flagIcons ? (
+                      <i className="fa-solid fa-heart"></i>
+                    ) : (
+                      <i className="fa-regular fa-heart"></i>
+                    )}
                   </button>
                 </div>
               );
-            }): 
-            <p className="textMsg">{noImgsMsg}</p>}
-          </div>
-          
+            })
+          ) : (
+            <p className="textMsg">{noImgsMsg}</p>
+          )}
         </div>
       </div>
+      <div>
+        {flagSug?<p className="title">Suggestions</p>:""}
+        <div>
+          <div className="images row m-auto">
+            {(suggestions)?
+            suggestions.map((item, idx) => {
+              return (
+                <div className="col-md-4 imageCard suggestion" key={idx} onClick={(e) => getImages(e, item.name, 100)}>
+                    <div className="member">
+                      <img
+                        src={`https://live.staticflickr.com/${item?.pic?.server}/${item?.pic?.id}_${item?.pic?.secret}_w.jpg`}
+                        alt={item?.pic?.title}
+                      />
+                      <div className="titleHover">
+                        <span class="nameHover">{item.name}</span>
+                      </div>
+
+                    </div>
+                </div>
+              );
+            }):<p>Nothing</p>}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/Utils/ApiUtils.jsx
+++ src/Utils/ApiUtils.jsx
@@ -3,11 +3,15 @@ import axios from "axios";
 export const apiKey = '32d46fe5e3f0a3c693e291271ef51e11';
 
 export async function ImageSearch(query, num, apiKey){
-    let resp = await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=${num}&format=json&nojsoncallback=1`);
-    if(resp.status === 200){
-      return resp.data;
-    } else{
-        console.log("NoResults in Api");
-    }
-
+    // let resp = await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=${num}&format=json&nojsoncallback=1`);
+    return await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=${num}&format=json&nojsoncallback=1`
+    ).then( async (resp)=>{
+      const status = resp.status
+      return {
+          data: await resp.data.photos,
+          status
+      } 
+  })
 }
+
+
